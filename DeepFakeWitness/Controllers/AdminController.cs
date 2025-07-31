3d58da850d1e2afdd27125880c87b2a0270be453
OwaisAslam911 <owaisaslams911@gmail.com>
2025-07-31 09:51:12 -0700

    update 1

--- DeepFakeWitness/Controllers/AdminController.cs
+++ DeepFakeWitness/Controllers/AdminController.cs
@@ -1,17 +1,67 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using DeepFakeWitness.Models;
+using Microsoft.Data.SqlClient;
+using Dapper;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
 
 namespace DeepFakeWitness.Controllers
 {
     public class AdminController : Controller
     {
+        private readonly IConfiguration config;
+
+        public AdminController(IConfiguration config)
+        {
+            this.config = config;
+        }
         public IActionResult Dashboard()
         {
-            return View();
+            try
+            {
+                var conn = config.GetConnectionString("dbcs");
+
+                using (var connection = new SqlConnection(conn))
+                {
+                    string query = "SELECT COUNT(*) FROM Users";
+                    int userCount = connection.ExecuteScalar<int>(query);
+
+                    ViewBag.UserCount = userCount;
+                }
+
+                return View();
+            }
+            catch (Exception ex)
+            {
+                ViewBag.UserCount = "Error";
+                ViewBag.ErrorMessage = ex.Message;
+                return View();
+            }
         }
+
+
+
+
+
+
+
+
         public IActionResult Users()
         {
             return View();
-        } 
+        }
+         public JsonResult GetUsers(Users users)
+        {
+            var conn = config.GetConnectionString("dbcs");
+            using (var connection = new SqlConnection (conn))
+            {
+                string UserQuery = "Select * from Users";
+                var result = connection.QueryFirstOrDefault(UserQuery);
+            return Json(result);
+            }
+        }
+
+
+
         public IActionResult Profile()
         {
             return View();
