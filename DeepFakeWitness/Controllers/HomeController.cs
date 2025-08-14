a46e5284dae3a7d3c64a65519b2d380fc5f40c61
OwaisAslam911 <owaisaslams911@gmail.com>
2025-08-14 11:11:11 -0700

    abc

--- DeepFakeWitness/Controllers/HomeController.cs
+++ DeepFakeWitness/Controllers/HomeController.cs
@@ -9,6 +9,8 @@ using Microsoft.Extensions.Logging;
 using System.Threading.Tasks;
 using System;
 using System.Linq;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.Data.SqlClient;
 
 namespace DeepFakeWitness.Controllers
 {
@@ -17,18 +19,69 @@ namespace DeepFakeWitness.Controllers
         private readonly ILogger<HomeController> _logger;
         private readonly IWebHostEnvironment _env;
         private readonly DeepFakeWitnessContext _context;
+        private readonly IConfiguration config;
 
-        public HomeController(ILogger<HomeController> logger, IWebHostEnvironment env, DeepFakeWitnessContext context)
+        public HomeController(ILogger<HomeController> logger, IWebHostEnvironment env, DeepFakeWitnessContext context, IConfiguration config)
         {
             _logger = logger;
             _env = env;
             _context = context;
+            this.config = config;
         }
-
         public IActionResult Index()
         {
+            //var conn = config.GetConnectionString("dbcs");
+            //using(var connection = new SqlConnection(conn))
+            //{
+            //    var role = HttpContext.Session.GetString("UserRole");
+            //if (role == "Admin")
+            //{
+            //    ViewData["Layout"] = "AdminLayout";
+            //}
+            //else
+            //{
+            //    ViewData["Layout"] = "_Layout";
+            //}
+            //return View();
+            //}
             return View();
         }
+        //[HttpPost]
+        //public async Task<JsonResult> CheckImage(IFormFile image)
+        //{
+        //    if (image != null && image.Length > 0)
+        //    {
+        //        var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+        //        string uploadsPath = Path.Combine(_env.WebRootPath, "uploads");
+        //        if (!Directory.Exists(uploadsPath))
+        //            Directory.CreateDirectory(uploadsPath);
+
+        //        string uniqueFileName = Guid.NewGuid().ToString() + Path.GetExtension(image.FileName);
+        //        string fullImagePath = Path.Combine(uploadsPath, uniqueFileName);
+
+        //        using (var stream = new FileStream(fullImagePath, FileMode.Create))
+        //        {
+        //            await image.CopyToAsync(stream);
+        //        }
+
+        //        string result = RunPythonDetection(fullImagePath);
+
+        //        var userImage = new UserImage
+        //        {
+        //            FileName = uniqueFileName,
+        //            UserId = userId,
+        //            DetectionResult = result
+        //        };
+        //        _context.UserImage.Add(userImage);
+        //        await _context.SaveChangesAsync();
+
+        //        return Json(new { success = true, message = result });
+        //    }
+
+        //    return Json(new { success = false, message = "Please upload a valid image." });
+        //}
+
         [HttpPost]
         public async Task<IActionResult> CheckImage(IFormFile image)
         {
