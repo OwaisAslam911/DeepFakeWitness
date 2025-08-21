0c5b3eaa138b5a69cfce06f8b5ab1726c89ce3c2
OwaisAslam911 <owaisaslams911@gmail.com>
2025-08-21 15:10:06 -0700

    contact and about completed

---
+++ DeepFakeWitness/Controllers/AdminProfileController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DeepFakeWitness.Controllers
+{
+    public class AdminProfileController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ DeepFakeWitness/Controllers/UserScanResultsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DeepFakeWitness.Controllers
+{
+    public class UserScanResultsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
